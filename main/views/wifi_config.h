9396a5a4d0e402a734409bb932cf9c11b217b576
netraular <netraular@gmail.com>
2025-02-23 05:52:33 +0100

    Añadida partición propia

---
+++ main/views/wifi_config.h
@@ -0,0 +1,8 @@
+#ifndef WIFI_CONFIG_H
+#define WIFI_CONFIG_H
+
+#define WIFI_SSID      "TuSSID"  // Reemplaza con tu SSID
+#define WIFI_PASSWORD  "TuContraseña" // Reemplaza con tu contraseña
+#define WIFI_MAX_RETRY  5
+
+#endif // WIFI_CONFIG_H
